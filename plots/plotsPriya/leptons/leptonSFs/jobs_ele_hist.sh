cf926f297542219d673fd7499f3e8c9fa9c9c810
priya sajid <priyasajid@hotmail.com>
2022-11-29 17:03:29 +0100

    adding the shell script

---
+++ plots/plotsPriya/leptons/leptonSFs/jobs_ele_hist.sh
@@ -0,0 +1,30 @@
+#python  mod_ele_hist.py Data Id
+#python  mod_ele_hist.py Data IdSpec
+#python  mod_ele_hist.py Data IpIso
+#
+#python  mod_ele_hist.py MC Id
+#python  mod_ele_hist.py MC IdSpec
+#python  mod_ele_hist.py MC IpIso
+
+#python  mod_ele_hist.py Data Id 2016
+#python  mod_ele_hist.py Data IdSpec 2016
+#python  mod_ele_hist.py Data IpIso 2016
+#python  mod_ele_hist.py MC Id 2016
+#python  mod_ele_hist.py MC IdSpec 2016
+#python  mod_ele_hist.py MC IpIso 2016
+
+python  mod_ele_hist.py Data Id 2017
+python  mod_ele_hist.py Data IdSpec 2017
+python  mod_ele_hist.py Data IpIso 2017
+
+python  mod_ele_hist.py MC Id 2017
+python  mod_ele_hist.py MC IdSpec 2017
+python  mod_ele_hist.py MC IpIso 2017
+
+
+python  mod_ele_hist.py Data Id 2018
+python  mod_ele_hist.py Data IdSpec 2018
+python  mod_ele_hist.py Data IpIso 2018
+python  mod_ele_hist.py MC Id 2018
+python  mod_ele_hist.py MC IdSpec 2018
+python  mod_ele_hist.py MC IpIso 2018
