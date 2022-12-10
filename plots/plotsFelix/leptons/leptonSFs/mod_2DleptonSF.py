6328ac1d83fb881d42e578da69e92f9c867b428a
Felix <felix.lang@services.vbc.ac.at>
2022-12-10 15:33:07 +0100

    changed paths

--- plots/plotsFelix/leptons/leptonSFs/mod_2DleptonSF.py
+++ plots/plotsFelix/leptons/leptonSFs/mod_2DleptonSF.py
@@ -119,7 +119,7 @@ else:
 
 	for i in range(nx):
 		i+=1
-		print nx
+		print h['m2p0_m2p5']
 		SF.SetBinContent(1, i, h['m2p0_m2p5'].GetBinContent(i))
 		print "value for 1st", h['m2p0_m2p5'].GetBinContent(i)
 		SF.SetBinError(1,   i, h['m2p0_m2p5'].GetBinError(i))
