cd7dd14b3b29f95a25750320094d15b6466ef976
Felix <felix.lang@services.vbc.ac.at>
2022-12-13 13:29:09 +0100

    changed jobs

--- plots/plotsFelix/leptons/leptonSFs/jobs.sh
+++ plots/plotsFelix/leptons/leptonSFs/jobs.sh
@@ -1,14 +1,14 @@
-# mod_ele_fit
+# JOBS mod_ele_fit
 
 #python  mod_ele_hist.py Data Id 2017
-python  mod_ele_hist.py Data IdSpec 2017
-python  mod_ele_hist.py Data IpIso 2017
+#python  mod_ele_hist.py Data IdSpec 2017
+#python  mod_ele_hist.py Data IpIso 2017
 #
 #python  mod_ele_hist.py MC Id 2017
-python  mod_ele_hist.py MC IdSpec 2017
-python  mod_ele_hist.py MC IpIso 2017
+#python  mod_ele_hist.py MC IdSpec 2017
+#python  mod_ele_hist.py MC IpIso 2017
 
-# mod_ele_fit
+# JOBS mod_ele_fit
 
 #python mod_ele_fit.py Data  Id  all 2017
 #python mod_ele_fit.py Data  Id  2p0_2p5 2017
@@ -34,51 +34,55 @@ python  mod_ele_hist.py MC IpIso 2017
 #python mod_ele_fit.py MC  Id  m0p8_m1p4 2017
 #python mod_ele_fit.py MC  Id  m0p8 2017
 #
-#python mod_ele_fit.py Data  IdSpec  all 2017
-#python mod_ele_fit.py Data  IdSpec  2p0_2p5 2017
-#python mod_ele_fit.py Data  IdSpec  1p5_2p0 2017
-#python mod_ele_fit.py Data  IdSpec  0p8_1p4 2017
-#python mod_ele_fit.py Data  IdSpec  0p8 2017
-#python mod_ele_fit.py Data  IdSpec  m2p0_m2p5 2017
-#python mod_ele_fit.py Data  IdSpec  m1p5_m2p0 2017
-#python mod_ele_fit.py Data  IdSpec  m0p8_m1p4 2017
-#python mod_ele_fit.py Data  IdSpec  m0p8 2017
-#
-#python mod_ele_fit.py MC  IdSpec  all 2017
-#python mod_ele_fit.py MC  IdSpec  2p0_2p5 2017
-#python mod_ele_fit.py MC  IdSpec  1p5_2p0 2017
-#python mod_ele_fit.py MC  IdSpec  0p8_1p4 2017
-#python mod_ele_fit.py MC  IdSpec  0p8 2017
-#python mod_ele_fit.py MC  IdSpec  m2p0_m2p5 2017
-#python mod_ele_fit.py MC  IdSpec  m1p5_m2p0 2017
-#python mod_ele_fit.py MC  IdSpec  m0p8_m1p4 2017
-#python mod_ele_fit.py MC  IdSpec  m0p8 2017
-#
-#python mod_ele_fit.py Data  IpIso  all 2017
-#python mod_ele_fit.py Data  IpIso  2p0_2p5 2017
-#python mod_ele_fit.py Data  IpIso  1p5_2p0 2017
-#python mod_ele_fit.py Data  IpIso  1p4_1p5 2017
-#python mod_ele_fit.py Data  IpIso  0p8_1p4 2017
-#python mod_ele_fit.py Data  IpIso  0p8 2017
-#python mod_ele_fit.py Data  IpIso  m2p0_m2p5 2017
-#python mod_ele_fit.py Data  IpIso  m1p5_m2p0 2017
-#python mod_ele_fit.py Data  IpIso  m0p8_m1p4 2017
-#python mod_ele_fit.py Data  IpIso  m1pm4_m1p5 2017
-#python mod_ele_fit.py Data  IpIso  m0p8 2017
-#
-#python mod_ele_fit.py MC  IpIso  all 2017
-#python mod_ele_fit.py MC  IpIso  2p0_2p5 2017
-#python mod_ele_fit.py MC  IpIso  1p5_2p0 2017
-#python mod_ele_fit.py MC  IpIso  1p4_1p5 2017
-#python mod_ele_fit.py MC  IpIso  0p8_1p4 2017
-#python mod_ele_fit.py MC  IpIso  0p8 2017
-#python mod_ele_fit.py MC  IpIso  m2p0_m2p5 2017
-#python mod_ele_fit.py MC  IpIso  m1p5_m2p0 2017
-#python mod_ele_fit.py MC  IpIso  m1pm4_m1p5 2017
-#python mod_ele_fit.py MC  IpIso  m0p8_m1p4 2017
-#python mod_ele_fit.py MC  IpIso  m0p8 2017
+python mod_ele_fit.py Data  IdSpec  all 2017
+python mod_ele_fit.py Data  IdSpec  2p0_2p5 2017
+python mod_ele_fit.py Data  IdSpec  1p5_2p0 2017
+python mod_ele_fit.py Data  IdSpec  1p4_1p5 2017
+python mod_ele_fit.py Data  IdSpec  0p8_1p4 2017
+python mod_ele_fit.py Data  IdSpec  0p8 2017
+python mod_ele_fit.py Data  IdSpec  m2p0_m2p5 2017
+python mod_ele_fit.py Data  IdSpec  m1p5_m2p0 2017
+python mod_ele_fit.py Data  IdSpec  m1pm4_m1p5 2017
+python mod_ele_fit.py Data  IdSpec  m0p8_m1p4 2017
+python mod_ele_fit.py Data  IdSpec  m0p8 2017
 
-# mod_ploteff
+python mod_ele_fit.py MC  IdSpec  all 2017
+python mod_ele_fit.py MC  IdSpec  2p0_2p5 2017
+python mod_ele_fit.py MC  IdSpec  1p5_2p0 2017
+python mod_ele_fit.py MC  IdSpec  1p4_1p5 2017
+python mod_ele_fit.py MC  IdSpec  0p8_1p4 2017
+python mod_ele_fit.py MC  IdSpec  0p8 2017
+python mod_ele_fit.py MC  IdSpec  m2p0_m2p5 2017
+python mod_ele_fit.py MC  IdSpec  m1p5_m2p0 2017
+python mod_ele_fit.py MC  IdSpec  m1pm4_m1p5 2017
+python mod_ele_fit.py MC  IdSpec  m0p8_m1p4 2017
+python mod_ele_fit.py MC  IdSpec  m0p8 2017
+
+python mod_ele_fit.py Data  IpIso  all 2017
+python mod_ele_fit.py Data  IpIso  2p0_2p5 2017
+python mod_ele_fit.py Data  IpIso  1p5_2p0 2017
+python mod_ele_fit.py Data  IpIso  1p4_1p5 2017
+python mod_ele_fit.py Data  IpIso  0p8_1p4 2017
+python mod_ele_fit.py Data  IpIso  0p8 2017
+python mod_ele_fit.py Data  IpIso  m2p0_m2p5 2017
+python mod_ele_fit.py Data  IpIso  m1p5_m2p0 2017
+python mod_ele_fit.py Data  IpIso  m0p8_m1p4 2017
+python mod_ele_fit.py Data  IpIso  m1pm4_m1p5 2017
+python mod_ele_fit.py Data  IpIso  m0p8 2017
+
+python mod_ele_fit.py MC  IpIso  all 2017
+python mod_ele_fit.py MC  IpIso  2p0_2p5 2017
+python mod_ele_fit.py MC  IpIso  1p5_2p0 2017
+python mod_ele_fit.py MC  IpIso  1p4_1p5 2017
+python mod_ele_fit.py MC  IpIso  0p8_1p4 2017
+python mod_ele_fit.py MC  IpIso  0p8 2017
+python mod_ele_fit.py MC  IpIso  m2p0_m2p5 2017
+python mod_ele_fit.py MC  IpIso  m1p5_m2p0 2017
+python mod_ele_fit.py MC  IpIso  m1pm4_m1p5 2017
+python mod_ele_fit.py MC  IpIso  m0p8_m1p4 2017
+python mod_ele_fit.py MC  IpIso  m0p8 2017
+
+# JOBS mod_ploteff
 
 #python mod_ploteff.py ele Id  all 2017
 #python mod_ploteff.py ele Id  2p0_2p5 2017
@@ -114,4 +118,8 @@ python  mod_ele_hist.py MC IpIso 2017
 #python mod_ploteff.py ele  IpIso  m1pm4_m1p5 2017
 #python mod_ploteff.py ele  IpIso  m0p8 2017
 
-# mod_2DleptonSF
+# JOBS mod_2DleptonSF
+
+#python mod_2DleptonSF.py hephy_scale_factors ele Id
+#python mod_2DleptonSF.py hephy_scale_factors ele IdSpec
+#python mod_2DleptonSF.py hephy_scale_factors ele IpIso
