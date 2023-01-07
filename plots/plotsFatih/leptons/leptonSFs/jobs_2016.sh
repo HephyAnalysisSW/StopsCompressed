d8be290396a0100d8729bff65c069e743a0874ab
fatih.okcu <fatih.okcu@clip-login-1.cbe.vbc.ac.at>
2023-01-07 19:27:38 +0100

    changed jobs

--- plots/plotsFatih/leptons/leptonSFs/jobs_2016.sh
+++ plots/plotsFatih/leptons/leptonSFs/jobs_2016.sh
@@ -7,161 +7,161 @@
 #python  mod_ele_hist.py MC IdSpec 2016 preVFP
 #python  mod_ele_hist.py MC IpIso 2016 preVFP
 
-python  mod_ele_hist.py Data Id 2016 postVFP
-python  mod_ele_hist.py Data IdSpec 2016 postVFP
-python  mod_ele_hist.py Data IpIso 2016 postVFP
-
-python  mod_ele_hist.py MC Id 2016 postVFP
-python  mod_ele_hist.py MC IdSpec 2016 postVFP
-python  mod_ele_hist.py MC IpIso 2016 postVFP
+#python  mod_ele_hist.py Data Id 2016 postVFP
+#python  mod_ele_hist.py Data IdSpec 2016 postVFP
+#python  mod_ele_hist.py Data IpIso 2016 postVFP
+#
+#python  mod_ele_hist.py MC Id 2016 postVFP
+#python  mod_ele_hist.py MC IdSpec 2016 postVFP
+#python  mod_ele_hist.py MC IpIso 2016 postVFP
 
 
 # JOBS FOR ELE FIT
-#python mod_ele_fit.py Data  Id  all 2016 preVFP
-#python mod_ele_fit.py Data  Id  2p0_2p5 2016 preVFP
-#python mod_ele_fit.py Data  Id  1p5_2p0 2016 preVFP
-#python mod_ele_fit.py Data  Id  1p4_1p5 2016 preVFP
-#python mod_ele_fit.py Data  Id  0p8_1p4 2016 preVFP
-#python mod_ele_fit.py Data  Id  0p8 2016 preVFP
-#python mod_ele_fit.py Data  Id  m2p0_m2p5 2016 preVFP
-#python mod_ele_fit.py Data  Id  m1p5_m2p0 2016 preVFP
-#python mod_ele_fit.py Data  Id  m1pm4_m1p5 2016 preVFP
-#python mod_ele_fit.py Data  Id  m0p8_m1p4 2016 preVFP
-#python mod_ele_fit.py Data  Id  m0p8 2016 preVFP
-#
-#python mod_ele_fit.py MC  Id  all 2016 preVFP
-#python mod_ele_fit.py MC  Id  2p0_2p5 2016 preVFP
-#python mod_ele_fit.py MC  Id  1p5_2p0 2016 preVFP
-#python mod_ele_fit.py MC  Id  1p4_1p5 2016 preVFP
-#python mod_ele_fit.py MC  Id  0p8_1p4 2016 preVFP
-#python mod_ele_fit.py MC  Id  0p8 2016 preVFP
-#python mod_ele_fit.py MC  Id  m2p0_m2p5 2016 preVFP
-#python mod_ele_fit.py MC  Id  m1p5_m2p0 2016 preVFP
-#python mod_ele_fit.py MC  Id  m1pm4_m1p5 2016 preVFP
-#python mod_ele_fit.py MC  Id  m0p8_m1p4 2016 preVFP
-#python mod_ele_fit.py MC  Id  m0p8 2016 preVFP
-#
-#python mod_ele_fit.py Data  IdSpec  all 2016 preVFP
-#python mod_ele_fit.py Data  IdSpec  2p0_2p5 2016 preVFP
-#python mod_ele_fit.py Data  IdSpec  1p5_2p0 2016 preVFP
-#python mod_ele_fit.py Data  IdSpec  1p4_1p5 2016 preVFP
-#python mod_ele_fit.py Data  IdSpec  0p8_1p4 2016 preVFP
-#python mod_ele_fit.py Data  IdSpec  0p8 2016 preVFP
-#python mod_ele_fit.py Data  IdSpec  m2p0_m2p5 2016 preVFP
-#python mod_ele_fit.py Data  IdSpec  m1p5_m2p0 2016 preVFP
-#python mod_ele_fit.py Data  IdSpec  m1pm4_m1p5 2016 preVFP
-#python mod_ele_fit.py Data  IdSpec  m0p8_m1p4 2016 preVFP
-#python mod_ele_fit.py Data  IdSpec  m0p8 2016 preVFP
-#
-#python mod_ele_fit.py MC  IdSpec  all 2016 preVFP
-#python mod_ele_fit.py MC  IdSpec  2p0_2p5 2016 preVFP
-#python mod_ele_fit.py MC  IdSpec  1p5_2p0 2016 preVFP
-#python mod_ele_fit.py MC  IdSpec  1p4_1p5 2016 preVFP
-#python mod_ele_fit.py MC  IdSpec  0p8_1p4 2016 preVFP
-#python mod_ele_fit.py MC  IdSpec  0p8 2016 preVFP
-#python mod_ele_fit.py MC  IdSpec  m2p0_m2p5 2016 preVFP
-#python mod_ele_fit.py MC  IdSpec  m1p5_m2p0 2016 preVFP
-#python mod_ele_fit.py MC  IdSpec  m1pm4_m1p5 2016 preVFP
-#python mod_ele_fit.py MC  IdSpec  m0p8_m1p4 2016 preVFP
-#python mod_ele_fit.py MC  IdSpec  m0p8 2016 preVFP
-#
-#python mod_ele_fit.py Data  IpIso  all 2016 preVFP
-#python mod_ele_fit.py Data  IpIso  2p0_2p5 2016 preVFP
-#python mod_ele_fit.py Data  IpIso  1p5_2p0 2016 preVFP
-#python mod_ele_fit.py Data  IpIso  1p4_1p5 2016 preVFP
-#python mod_ele_fit.py Data  IpIso  0p8_1p4 2016 preVFP
-#python mod_ele_fit.py Data  IpIso  0p8 2016 preVFP
-#python mod_ele_fit.py Data  IpIso  m2p0_m2p5 2016 preVFP
-#python mod_ele_fit.py Data  IpIso  m1p5_m2p0 2016 preVFP
-#python mod_ele_fit.py Data  IpIso  m0p8_m1p4 2016 preVFP
-#python mod_ele_fit.py Data  IpIso  m1pm4_m1p5 2016 preVFP
-#python mod_ele_fit.py Data  IpIso  m0p8 2016 preVFP
-#
-#python mod_ele_fit.py MC  IpIso  all 2016 preVFP
-#python mod_ele_fit.py MC  IpIso  2p0_2p5 2016 preVFP
-#python mod_ele_fit.py MC  IpIso  1p5_2p0 2016 preVFP
-#python mod_ele_fit.py MC  IpIso  1p4_1p5 2016 preVFP
-#python mod_ele_fit.py MC  IpIso  0p8_1p4 2016 preVFP
-#python mod_ele_fit.py MC  IpIso  0p8 2016 preVFP
-#python mod_ele_fit.py MC  IpIso  m2p0_m2p5 2016 preVFP
-#python mod_ele_fit.py MC  IpIso  m1p5_m2p0 2016 preVFP
-#python mod_ele_fit.py MC  IpIso  m1pm4_m1p5 2016 preVFP
-#python mod_ele_fit.py MC  IpIso  m0p8_m1p4 2016 preVFP
-#python mod_ele_fit.py MC  IpIso  m0p8 2016 preVFP
-#
-#
-#
-#python mod_ele_fit.py Data  Id  all 2016 postVFP
-#python mod_ele_fit.py Data  Id  2p0_2p5 2016 postVFP
-#python mod_ele_fit.py Data  Id  1p5_2p0 2016 postVFP
-#python mod_ele_fit.py Data  Id  1p4_1p5 2016 postVFP
-#python mod_ele_fit.py Data  Id  0p8_1p4 2016 postVFP
-#python mod_ele_fit.py Data  Id  0p8 2016 postVFP
-#python mod_ele_fit.py Data  Id  m2p0_m2p5 2016 postVFP
-#python mod_ele_fit.py Data  Id  m1p5_m2p0 2016 postVFP
-#python mod_ele_fit.py Data  Id  m1pm4_m1p5 2016 postVFP
-#python mod_ele_fit.py Data  Id  m0p8_m1p4 2016 postVFP
-#python mod_ele_fit.py Data  Id  m0p8 2016 postVFP
-#
-#python mod_ele_fit.py MC  Id  all 2016 postVFP
-#python mod_ele_fit.py MC  Id  2p0_2p5 2016 postVFP
-#python mod_ele_fit.py MC  Id  1p5_2p0 2016 postVFP
-#python mod_ele_fit.py MC  Id  1p4_1p5 2016 postVFP
-#python mod_ele_fit.py MC  Id  0p8_1p4 2016 postVFP
-#python mod_ele_fit.py MC  Id  0p8 2016 postVFP
-#python mod_ele_fit.py MC  Id  m2p0_m2p5 2016 postVFP
-#python mod_ele_fit.py MC  Id  m1p5_m2p0 2016 postVFP
-#python mod_ele_fit.py MC  Id  m1pm4_m1p5 2016 postVFP
-#python mod_ele_fit.py MC  Id  m0p8_m1p4 2016 postVFP
-#python mod_ele_fit.py MC  Id  m0p8 2016 postVFP
-#
-#python mod_ele_fit.py Data  IdSpec  all 2016 postVFP
-#python mod_ele_fit.py Data  IdSpec  2p0_2p5 2016 postVFP
-#python mod_ele_fit.py Data  IdSpec  1p5_2p0 2016 postVFP
-#python mod_ele_fit.py Data  IdSpec  1p4_1p5 2016 postVFP
-#python mod_ele_fit.py Data  IdSpec  0p8_1p4 2016 postVFP
-#python mod_ele_fit.py Data  IdSpec  0p8 2016 postVFP
-#python mod_ele_fit.py Data  IdSpec  m2p0_m2p5 2016 postVFP
-#python mod_ele_fit.py Data  IdSpec  m1p5_m2p0 2016 postVFP
-#python mod_ele_fit.py Data  IdSpec  m1pm4_m1p5 2016 postVFP
-#python mod_ele_fit.py Data  IdSpec  m0p8_m1p4 2016 postVFP
-#python mod_ele_fit.py Data  IdSpec  m0p8 2016 postVFP
-#
-#python mod_ele_fit.py MC  IdSpec  all 2016 postVFP
-#python mod_ele_fit.py MC  IdSpec  2p0_2p5 2016 postVFP
-#python mod_ele_fit.py MC  IdSpec  1p5_2p0 2016 postVFP
-#python mod_ele_fit.py MC  IdSpec  1p4_1p5 2016 postVFP
-#python mod_ele_fit.py MC  IdSpec  0p8_1p4 2016 postVFP
-#python mod_ele_fit.py MC  IdSpec  0p8 2016 postVFP
-#python mod_ele_fit.py MC  IdSpec  m2p0_m2p5 2016 postVFP
-#python mod_ele_fit.py MC  IdSpec  m1p5_m2p0 2016 postVFP
-#python mod_ele_fit.py MC  IdSpec  m1pm4_m1p5 2016 postVFP
-#python mod_ele_fit.py MC  IdSpec  m0p8_m1p4 2016 postVFP
-#python mod_ele_fit.py MC  IdSpec  m0p8 2016 postVFP
-#
-#python mod_ele_fit.py Data  IpIso  all 2016 postVFP
-#python mod_ele_fit.py Data  IpIso  2p0_2p5 2016 postVFP
-#python mod_ele_fit.py Data  IpIso  1p5_2p0 2016 postVFP
-#python mod_ele_fit.py Data  IpIso  1p4_1p5 2016 postVFP
-#python mod_ele_fit.py Data  IpIso  0p8_1p4 2016 postVFP
-#python mod_ele_fit.py Data  IpIso  0p8 2016 postVFP
-#python mod_ele_fit.py Data  IpIso  m2p0_m2p5 2016 postVFP
-#python mod_ele_fit.py Data  IpIso  m1p5_m2p0 2016 postVFP
-#python mod_ele_fit.py Data  IpIso  m0p8_m1p4 2016 postVFP
-#python mod_ele_fit.py Data  IpIso  m1pm4_m1p5 2016 postVFP
-#python mod_ele_fit.py Data  IpIso  m0p8 2016 postVFP
-#
-#python mod_ele_fit.py MC  IpIso  all 2016 postVFP
-#python mod_ele_fit.py MC  IpIso  2p0_2p5 2016 postVFP
-#python mod_ele_fit.py MC  IpIso  1p5_2p0 2016 postVFP
-#python mod_ele_fit.py MC  IpIso  1p4_1p5 2016 postVFP
-#python mod_ele_fit.py MC  IpIso  0p8_1p4 2016 postVFP
-#python mod_ele_fit.py MC  IpIso  0p8 2016 postVFP
-#python mod_ele_fit.py MC  IpIso  m2p0_m2p5 2016 postVFP
-#python mod_ele_fit.py MC  IpIso  m1p5_m2p0 2016 postVFP
-#python mod_ele_fit.py MC  IpIso  m1pm4_m1p5 2016 postVFP
-#python mod_ele_fit.py MC  IpIso  m0p8_m1p4 2016 postVFP
-#python mod_ele_fit.py MC  IpIso  m0p8 2016 postVFP
+python mod_ele_fit.py Data  Id  all 2016 preVFP
+python mod_ele_fit.py Data  Id  2p0_2p5 2016 preVFP
+python mod_ele_fit.py Data  Id  1p5_2p0 2016 preVFP
+python mod_ele_fit.py Data  Id  1p4_1p5 2016 preVFP
+python mod_ele_fit.py Data  Id  0p8_1p4 2016 preVFP
+python mod_ele_fit.py Data  Id  0p8 2016 preVFP
+python mod_ele_fit.py Data  Id  m2p0_m2p5 2016 preVFP
+python mod_ele_fit.py Data  Id  m1p5_m2p0 2016 preVFP
+python mod_ele_fit.py Data  Id  m1pm4_m1p5 2016 preVFP
+python mod_ele_fit.py Data  Id  m0p8_m1p4 2016 preVFP
+python mod_ele_fit.py Data  Id  m0p8 2016 preVFP
+
+python mod_ele_fit.py MC  Id  all 2016 preVFP
+python mod_ele_fit.py MC  Id  2p0_2p5 2016 preVFP
+python mod_ele_fit.py MC  Id  1p5_2p0 2016 preVFP
+python mod_ele_fit.py MC  Id  1p4_1p5 2016 preVFP
+python mod_ele_fit.py MC  Id  0p8_1p4 2016 preVFP
+python mod_ele_fit.py MC  Id  0p8 2016 preVFP
+python mod_ele_fit.py MC  Id  m2p0_m2p5 2016 preVFP
+python mod_ele_fit.py MC  Id  m1p5_m2p0 2016 preVFP
+python mod_ele_fit.py MC  Id  m1pm4_m1p5 2016 preVFP
+python mod_ele_fit.py MC  Id  m0p8_m1p4 2016 preVFP
+python mod_ele_fit.py MC  Id  m0p8 2016 preVFP
+
+python mod_ele_fit.py Data  IdSpec  all 2016 preVFP
+python mod_ele_fit.py Data  IdSpec  2p0_2p5 2016 preVFP
+python mod_ele_fit.py Data  IdSpec  1p5_2p0 2016 preVFP
+python mod_ele_fit.py Data  IdSpec  1p4_1p5 2016 preVFP
+python mod_ele_fit.py Data  IdSpec  0p8_1p4 2016 preVFP
+python mod_ele_fit.py Data  IdSpec  0p8 2016 preVFP
+python mod_ele_fit.py Data  IdSpec  m2p0_m2p5 2016 preVFP
+python mod_ele_fit.py Data  IdSpec  m1p5_m2p0 2016 preVFP
+python mod_ele_fit.py Data  IdSpec  m1pm4_m1p5 2016 preVFP
+python mod_ele_fit.py Data  IdSpec  m0p8_m1p4 2016 preVFP
+python mod_ele_fit.py Data  IdSpec  m0p8 2016 preVFP
+
+python mod_ele_fit.py MC  IdSpec  all 2016 preVFP
+python mod_ele_fit.py MC  IdSpec  2p0_2p5 2016 preVFP
+python mod_ele_fit.py MC  IdSpec  1p5_2p0 2016 preVFP
+python mod_ele_fit.py MC  IdSpec  1p4_1p5 2016 preVFP
+python mod_ele_fit.py MC  IdSpec  0p8_1p4 2016 preVFP
+python mod_ele_fit.py MC  IdSpec  0p8 2016 preVFP
+python mod_ele_fit.py MC  IdSpec  m2p0_m2p5 2016 preVFP
+python mod_ele_fit.py MC  IdSpec  m1p5_m2p0 2016 preVFP
+python mod_ele_fit.py MC  IdSpec  m1pm4_m1p5 2016 preVFP
+python mod_ele_fit.py MC  IdSpec  m0p8_m1p4 2016 preVFP
+python mod_ele_fit.py MC  IdSpec  m0p8 2016 preVFP
+
+python mod_ele_fit.py Data  IpIso  all 2016 preVFP
+python mod_ele_fit.py Data  IpIso  2p0_2p5 2016 preVFP
+python mod_ele_fit.py Data  IpIso  1p5_2p0 2016 preVFP
+python mod_ele_fit.py Data  IpIso  1p4_1p5 2016 preVFP
+python mod_ele_fit.py Data  IpIso  0p8_1p4 2016 preVFP
+python mod_ele_fit.py Data  IpIso  0p8 2016 preVFP
+python mod_ele_fit.py Data  IpIso  m2p0_m2p5 2016 preVFP
+python mod_ele_fit.py Data  IpIso  m1p5_m2p0 2016 preVFP
+python mod_ele_fit.py Data  IpIso  m0p8_m1p4 2016 preVFP
+python mod_ele_fit.py Data  IpIso  m1pm4_m1p5 2016 preVFP
+python mod_ele_fit.py Data  IpIso  m0p8 2016 preVFP
+
+python mod_ele_fit.py MC  IpIso  all 2016 preVFP
+python mod_ele_fit.py MC  IpIso  2p0_2p5 2016 preVFP
+python mod_ele_fit.py MC  IpIso  1p5_2p0 2016 preVFP
+python mod_ele_fit.py MC  IpIso  1p4_1p5 2016 preVFP
+python mod_ele_fit.py MC  IpIso  0p8_1p4 2016 preVFP
+python mod_ele_fit.py MC  IpIso  0p8 2016 preVFP
+python mod_ele_fit.py MC  IpIso  m2p0_m2p5 2016 preVFP
+python mod_ele_fit.py MC  IpIso  m1p5_m2p0 2016 preVFP
+python mod_ele_fit.py MC  IpIso  m1pm4_m1p5 2016 preVFP
+python mod_ele_fit.py MC  IpIso  m0p8_m1p4 2016 preVFP
+python mod_ele_fit.py MC  IpIso  m0p8 2016 preVFP
+
+
+
+python mod_ele_fit.py Data  Id  all 2016 postVFP
+python mod_ele_fit.py Data  Id  2p0_2p5 2016 postVFP
+python mod_ele_fit.py Data  Id  1p5_2p0 2016 postVFP
+python mod_ele_fit.py Data  Id  1p4_1p5 2016 postVFP
+python mod_ele_fit.py Data  Id  0p8_1p4 2016 postVFP
+python mod_ele_fit.py Data  Id  0p8 2016 postVFP
+python mod_ele_fit.py Data  Id  m2p0_m2p5 2016 postVFP
+python mod_ele_fit.py Data  Id  m1p5_m2p0 2016 postVFP
+python mod_ele_fit.py Data  Id  m1pm4_m1p5 2016 postVFP
+python mod_ele_fit.py Data  Id  m0p8_m1p4 2016 postVFP
+python mod_ele_fit.py Data  Id  m0p8 2016 postVFP
+
+python mod_ele_fit.py MC  Id  all 2016 postVFP
+python mod_ele_fit.py MC  Id  2p0_2p5 2016 postVFP
+python mod_ele_fit.py MC  Id  1p5_2p0 2016 postVFP
+python mod_ele_fit.py MC  Id  1p4_1p5 2016 postVFP
+python mod_ele_fit.py MC  Id  0p8_1p4 2016 postVFP
+python mod_ele_fit.py MC  Id  0p8 2016 postVFP
+python mod_ele_fit.py MC  Id  m2p0_m2p5 2016 postVFP
+python mod_ele_fit.py MC  Id  m1p5_m2p0 2016 postVFP
+python mod_ele_fit.py MC  Id  m1pm4_m1p5 2016 postVFP
+python mod_ele_fit.py MC  Id  m0p8_m1p4 2016 postVFP
+python mod_ele_fit.py MC  Id  m0p8 2016 postVFP
+
+python mod_ele_fit.py Data  IdSpec  all 2016 postVFP
+python mod_ele_fit.py Data  IdSpec  2p0_2p5 2016 postVFP
+python mod_ele_fit.py Data  IdSpec  1p5_2p0 2016 postVFP
+python mod_ele_fit.py Data  IdSpec  1p4_1p5 2016 postVFP
+python mod_ele_fit.py Data  IdSpec  0p8_1p4 2016 postVFP
+python mod_ele_fit.py Data  IdSpec  0p8 2016 postVFP
+python mod_ele_fit.py Data  IdSpec  m2p0_m2p5 2016 postVFP
+python mod_ele_fit.py Data  IdSpec  m1p5_m2p0 2016 postVFP
+python mod_ele_fit.py Data  IdSpec  m1pm4_m1p5 2016 postVFP
+python mod_ele_fit.py Data  IdSpec  m0p8_m1p4 2016 postVFP
+python mod_ele_fit.py Data  IdSpec  m0p8 2016 postVFP
+
+python mod_ele_fit.py MC  IdSpec  all 2016 postVFP
+python mod_ele_fit.py MC  IdSpec  2p0_2p5 2016 postVFP
+python mod_ele_fit.py MC  IdSpec  1p5_2p0 2016 postVFP
+python mod_ele_fit.py MC  IdSpec  1p4_1p5 2016 postVFP
+python mod_ele_fit.py MC  IdSpec  0p8_1p4 2016 postVFP
+python mod_ele_fit.py MC  IdSpec  0p8 2016 postVFP
+python mod_ele_fit.py MC  IdSpec  m2p0_m2p5 2016 postVFP
+python mod_ele_fit.py MC  IdSpec  m1p5_m2p0 2016 postVFP
+python mod_ele_fit.py MC  IdSpec  m1pm4_m1p5 2016 postVFP
+python mod_ele_fit.py MC  IdSpec  m0p8_m1p4 2016 postVFP
+python mod_ele_fit.py MC  IdSpec  m0p8 2016 postVFP
+
+python mod_ele_fit.py Data  IpIso  all 2016 postVFP
+python mod_ele_fit.py Data  IpIso  2p0_2p5 2016 postVFP
+python mod_ele_fit.py Data  IpIso  1p5_2p0 2016 postVFP
+python mod_ele_fit.py Data  IpIso  1p4_1p5 2016 postVFP
+python mod_ele_fit.py Data  IpIso  0p8_1p4 2016 postVFP
+python mod_ele_fit.py Data  IpIso  0p8 2016 postVFP
+python mod_ele_fit.py Data  IpIso  m2p0_m2p5 2016 postVFP
+python mod_ele_fit.py Data  IpIso  m1p5_m2p0 2016 postVFP
+python mod_ele_fit.py Data  IpIso  m0p8_m1p4 2016 postVFP
+python mod_ele_fit.py Data  IpIso  m1pm4_m1p5 2016 postVFP
+python mod_ele_fit.py Data  IpIso  m0p8 2016 postVFP
+
+python mod_ele_fit.py MC  IpIso  all 2016 postVFP
+python mod_ele_fit.py MC  IpIso  2p0_2p5 2016 postVFP
+python mod_ele_fit.py MC  IpIso  1p5_2p0 2016 postVFP
+python mod_ele_fit.py MC  IpIso  1p4_1p5 2016 postVFP
+python mod_ele_fit.py MC  IpIso  0p8_1p4 2016 postVFP
+python mod_ele_fit.py MC  IpIso  0p8 2016 postVFP
+python mod_ele_fit.py MC  IpIso  m2p0_m2p5 2016 postVFP
+python mod_ele_fit.py MC  IpIso  m1p5_m2p0 2016 postVFP
+python mod_ele_fit.py MC  IpIso  m1pm4_m1p5 2016 postVFP
+python mod_ele_fit.py MC  IpIso  m0p8_m1p4 2016 postVFP
+python mod_ele_fit.py MC  IpIso  m0p8 2016 postVFP
 
 # JOBS FOR mod_ploteff.py
 #python mod_ploteff.py ele IpIso all 2016 preVFP
