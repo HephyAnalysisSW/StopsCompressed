c22a04345c8a07e19d1622bc6ba34cba082a2795
Felix <felix.lang@services.vbc.ac.at>
2023-01-17 16:02:26 +0100

    changed paths

--- plots/plotsFelix/leptons/leptonSFs/mod_muon_hist.py
+++ plots/plotsFelix/leptons/leptonSFs/mod_muon_hist.py
@@ -113,16 +113,16 @@ elif year =="2017":
 	    t.Add("/scratch/priya.hussain/StopsCompressed/TnP/Run2017/94X/MC/TnPTree_94X_DYJetsToLL_M50_Madgraph.root")
 elif year == "2018":
 	if mode == "Data":
-	    t.Add("/scratch/priya.hussain/StopsCompressed/TnP/Run2018/94_pre3/TnPTreeZ_17Sep2018_SingleMuon_Run2018Av2_GoldenJSON.root")
-	    t.Add("/scratch/priya.hussain/StopsCompressed/TnP/Run2018/94_pre3/TnPTreeZ_17Sep2018_SingleMuon_Run2018Bv1_GoldenJSON.root")
-	    t.Add("/scratch/priya.hussain/StopsCompressed/TnP/Run2018/94_pre3/TnPTreeZ_17Sep2018_SingleMuon_Run2018Cv1_GoldenJSON.root")
-	    t.Add("/scratch/priya.hussain/StopsCompressed/TnP/Run2018/94_pre3/TnPTreeZ_SingleMuon_Run2018Dv2_GoldenJSON_Upto323523.root")
+	    t.Add("/groups/hephy/cms/priya.hussain/StopsCompressed/TnP/UL2018_Muon/SingleMuon_Run2018A/tnpZ_Data_hadded.root")
+	    t.Add("/groups/hephy/cms/priya.hussain/StopsCompressed/TnP/UL2018_Muon/SingleMuon_Run2018B/tnpZ_Data_hadded.root")
+	    t.Add("/groups/hephy/cms/priya.hussain/StopsCompressed/TnP/UL2018_Muon/SingleMuon_Run2018C/tnpZ_Data_hadded.root")
+	    t.Add("/groups/hephy/cms/priya.hussain/StopsCompressed/TnP/UL2018_Muon/SingleMuon_Run2018D/tnpZ_Data_hadded.root")
 	else:
-	    t.Add("/scratch/priya.hussain/StopsCompressed/TnP/Run2018/94_pre3/TnPTreeZ_102XAutumn18_DYJetsToLL_M50_MadgraphMLM.root")
+	    t.Add("/groups/hephy/cms/priya.hussain/StopsCompressed/TnP/UL2018_Muon/DY_M50_Madgraph_STA/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_UL18MC.root")
 
-makeDir("/scratch/priya.hussain/StopsCompressed/results/%s/hists"%datatag) 
+makeDir("/groups/hephy/cms/felix.lang/StopsCompressed/results/%s/hists"%datatag)
 #fout = TFile("/scratch/priya.hussain/StopsCompressed/results/%s/hists/mu_hists_%s_%s.root"%(datatag,mode,stage),"recreate")
-fout = TFile("/scratch/priya.hussain/StopsCompressed/results/%s/hists/mu_hists_%s_%s.root"%(datatag,mode,stage),"update")
+fout = TFile("/groups/hephy/cms/felix.lang/StopsCompressed/results/%s/hists/mu_hists_%s_%s.root"%(datatag,mode,stage),"update")
 
 hlist = []
 
