53dfbbaa3816d8fb756a1f5960614f00459477c0
Felix <felix.lang@services.vbc.ac.at>
2023-01-17 16:10:10 +0100

    updated jobs

--- plots/plotsFelix/leptons/leptonSFs/jobs_muon_2018.sh
+++ plots/plotsFelix/leptons/leptonSFs/jobs_muon_2018.sh
@@ -1,1 +1,6 @@
-
+python mod_muon_hist.py Data Id 2018
+python mod_muon_hist.py Data IpIso 2018
+python mod_muon_hist.py Data IpIsoSpec 2018
+python mod_muon_hist.py MC   Id 2018
+python mod_muon_hist.py MC   IpIso 2018
+python mod_muon_hist.py MC   IpIsoSpec 2018
