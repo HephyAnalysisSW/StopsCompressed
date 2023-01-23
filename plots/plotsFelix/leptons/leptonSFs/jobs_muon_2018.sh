a5c0fdf4988d85503b3d852519440aabe22e6ebe
Felix <felix.lang@services.vbc.ac.at>
2023-01-23 19:06:09 +0100

    changed etabins

--- plots/plotsFelix/leptons/leptonSFs/jobs_muon_2018.sh
+++ plots/plotsFelix/leptons/leptonSFs/jobs_muon_2018.sh
@@ -9,38 +9,38 @@
 
 # JOBS mod_muon_fit
 
-python mod_muon_fit.py Data  Id  all 2018
-python mod_muon_fit.py Data  Id  0p9 2018
-python mod_muon_fit.py Data  Id  0p9_1p2 2018
-python mod_muon_fit.py Data  Id  1p2_2p1 2018
-python mod_muon_fit.py Data  Id  2p1_2p4 2018
-
-python mod_muon_fit.py MC  Id  all 2018
-python mod_muon_fit.py MC  Id  0p9 2018
-python mod_muon_fit.py MC  Id  0p9_1p2 2018
-python mod_muon_fit.py MC  Id  1p2_2p1 2018
-python mod_muon_fit.py MC  Id  2p1_2p4 2018
-
-python mod_muon_fit.py Data  IpIso  all 2018
-python mod_muon_fit.py Data  IpIso  0p9 2018
-python mod_muon_fit.py Data  IpIso  0p9_1p2 2018
-python mod_muon_fit.py Data  IpIso  1p2_2p1 2018
-python mod_muon_fit.py Data  IpIso  2p1_2p4 2018
-
-python mod_muon_fit.py MC  IpIso  all 2018
-python mod_muon_fit.py MC  IpIso  0p9 2018
-python mod_muon_fit.py MC  IpIso  0p9_1p2 2018
-python mod_muon_fit.py MC  IpIso  1p2_2p1 2018
-python mod_muon_fit.py MC  IpIso  2p1_2p4 2018
-
-python mod_muon_fit.py Data  IpIsoSpec  all 2018
-python mod_muon_fit.py Data  IpIsoSpec  0p9 2018
-python mod_muon_fit.py Data  IpIsoSpec  0p9_1p2 2018
-python mod_muon_fit.py Data  IpIsoSpec  1p2_2p1 2018
-python mod_muon_fit.py Data  IpIsoSpec  2p1_2p4 2018
-
-python mod_muon_fit.py MC  IpIsoSpec  all 2018
-python mod_muon_fit.py MC  IpIsoSpec  0p9 2018
-python mod_muon_fit.py MC  IpIsoSpec  0p9_1p2 2018
-python mod_muon_fit.py MC  IpIsoSpec  1p2_2p1 2018
-python mod_muon_fit.py MC  IpIsoSpec  2p1_2p4 2018
+#python mod_muon_fit.py Data  Id  all 2018
+#python mod_muon_fit.py Data  Id  0p9 2018
+#python mod_muon_fit.py Data  Id  0p9_1p2 2018
+#python mod_muon_fit.py Data  Id  1p2_2p1 2018
+#python mod_muon_fit.py Data  Id  2p1_2p4 2018
+#
+#python mod_muon_fit.py MC  Id  all 2018
+#python mod_muon_fit.py MC  Id  0p9 2018
+#python mod_muon_fit.py MC  Id  0p9_1p2 2018
+#python mod_muon_fit.py MC  Id  1p2_2p1 2018
+#python mod_muon_fit.py MC  Id  2p1_2p4 2018
+#
+#python mod_muon_fit.py Data  IpIso  all 2018
+#python mod_muon_fit.py Data  IpIso  0p9 2018
+#python mod_muon_fit.py Data  IpIso  0p9_1p2 2018
+#python mod_muon_fit.py Data  IpIso  1p2_2p1 2018
+#python mod_muon_fit.py Data  IpIso  2p1_2p4 2018
+#
+#python mod_muon_fit.py MC  IpIso  all 2018
+#python mod_muon_fit.py MC  IpIso  0p9 2018
+#python mod_muon_fit.py MC  IpIso  0p9_1p2 2018
+#python mod_muon_fit.py MC  IpIso  1p2_2p1 2018
+#python mod_muon_fit.py MC  IpIso  2p1_2p4 2018
+#
+#python mod_muon_fit.py Data  IpIsoSpec  all 2018
+#python mod_muon_fit.py Data  IpIsoSpec  0p9 2018
+#python mod_muon_fit.py Data  IpIsoSpec  0p9_1p2 2018
+#python mod_muon_fit.py Data  IpIsoSpec  1p2_2p1 2018
+#python mod_muon_fit.py Data  IpIsoSpec  2p1_2p4 2018
+#
+#python mod_muon_fit.py MC  IpIsoSpec  all 2018
+#python mod_muon_fit.py MC  IpIsoSpec  0p9 2018
+#python mod_muon_fit.py MC  IpIsoSpec  0p9_1p2 2018
+#python mod_muon_fit.py MC  IpIsoSpec  1p2_2p1 2018
+#python mod_muon_fit.py MC  IpIsoSpec  2p1_2p4 2018
