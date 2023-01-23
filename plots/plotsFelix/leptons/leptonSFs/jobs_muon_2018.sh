ccf8f6282276d52363297de18a3135c935a40b62
Felix <felix.lang@services.vbc.ac.at>
2023-01-23 18:03:50 +0100

    updated jobs

--- plots/plotsFelix/leptons/leptonSFs/jobs_muon_2018.sh
+++ plots/plotsFelix/leptons/leptonSFs/jobs_muon_2018.sh
@@ -1,4 +1,4 @@
-# JOBS mod_ele_fit
+# JOBS mod_muon_hist
 
 #python mod_muon_hist.py Data Id 2018
 #python mod_muon_hist.py Data IpIso 2018
@@ -7,3 +7,40 @@
 #python mod_muon_hist.py MC   IpIso 2018
 #python mod_muon_hist.py MC   IpIsoSpec 2018
 
+# JOBS mod_muon_fit
+
+python mod_muon_fit.py Data  Id  all 2018
+python mod_muon_fit.py Data  Id  0p9 2018
+python mod_muon_fit.py Data  Id  0p9_1p2 2018
+python mod_muon_fit.py Data  Id  1p2_2p1 2018
+python mod_muon_fit.py Data  Id  2p1_2p4 2018
+
+python mod_muon_fit.py MC  Id  all 2018
+python mod_muon_fit.py MC  Id  0p9 2018
+python mod_muon_fit.py MC  Id  0p9_1p2 2018
+python mod_muon_fit.py MC  Id  1p2_2p1 2018
+python mod_muon_fit.py MC  Id  2p1_2p4 2018
+
+python mod_muon_fit.py Data  IpIso  all 2018
+python mod_muon_fit.py Data  IpIso  0p9 2018
+python mod_muon_fit.py Data  IpIso  0p9_1p2 2018
+python mod_muon_fit.py Data  IpIso  1p2_2p1 2018
+python mod_muon_fit.py Data  IpIso  2p1_2p4 2018
+
+python mod_muon_fit.py MC  IpIso  all 2018
+python mod_muon_fit.py MC  IpIso  0p9 2018
+python mod_muon_fit.py MC  IpIso  0p9_1p2 2018
+python mod_muon_fit.py MC  IpIso  1p2_2p1 2018
+python mod_muon_fit.py MC  IpIso  2p1_2p4 2018
+
+python mod_muon_fit.py Data  IpIsoSpec  all 2018
+python mod_muon_fit.py Data  IpIsoSpec  0p9 2018
+python mod_muon_fit.py Data  IpIsoSpec  0p9_1p2 2018
+python mod_muon_fit.py Data  IpIsoSpec  1p2_2p1 2018
+python mod_muon_fit.py Data  IpIsoSpec  2p1_2p4 2018
+
+python mod_muon_fit.py MC  IpIsoSpec  all 2018
+python mod_muon_fit.py MC  IpIsoSpec  0p9 2018
+python mod_muon_fit.py MC  IpIsoSpec  0p9_1p2 2018
+python mod_muon_fit.py MC  IpIsoSpec  1p2_2p1 2018
+python mod_muon_fit.py MC  IpIsoSpec  2p1_2p4 2018
