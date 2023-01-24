2b5367011d256f715e4b01e5428045c8beb19f9c
Felix <felix.lang@services.vbc.ac.at>
2023-01-24 13:06:43 +0100

    changed etabins

--- plots/plotsFelix/leptons/leptonSFs/jobs_muon_2018.sh
+++ plots/plotsFelix/leptons/leptonSFs/jobs_muon_2018.sh
@@ -1,11 +1,11 @@
 # JOBS mod_muon_hist
 
-#python mod_muon_hist.py Data Id 2018
-#python mod_muon_hist.py Data IpIso 2018
-#python mod_muon_hist.py Data IpIsoSpec 2018
-#python mod_muon_hist.py MC   Id 2018
-#python mod_muon_hist.py MC   IpIso 2018
-#python mod_muon_hist.py MC   IpIsoSpec 2018
+python mod_muon_hist.py Data Id 2018
+python mod_muon_hist.py Data IpIso 2018
+python mod_muon_hist.py Data IpIsoSpec 2018
+python mod_muon_hist.py MC   Id 2018
+python mod_muon_hist.py MC   IpIso 2018
+python mod_muon_hist.py MC   IpIsoSpec 2018
 
 # JOBS mod_muon_fit
 
