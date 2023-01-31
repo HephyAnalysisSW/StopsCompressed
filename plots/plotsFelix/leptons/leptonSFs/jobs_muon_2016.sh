10160ead47534d828a543130db2c1f0fa6dc70f3
Felix <felix.lang@services.vbc.ac.at>
2023-01-31 14:48:56 +0100

    changed jobs

--- plots/plotsFelix/leptons/leptonSFs/jobs_muon_2016.sh
+++ plots/plotsFelix/leptons/leptonSFs/jobs_muon_2016.sh
@@ -1,1 +1,15 @@
+# JOBS mod_muon_hist
 
+python mod_muon_hist.py Data Id 2016 preVFP
+python mod_muon_hist.py Data IpIso 2016 preVFP
+python mod_muon_hist.py Data IpIsoSpec 2016 preVFP
+python mod_muon_hist.py MC Id 2016 preVFP
+python mod_muon_hist.py MC IpIso 2016 preVFP
+python mod_muon_hist.py MC IpIsoSpec 2016 preVFP
+
+python mod_muon_hist.py Data Id 2016 postVFP
+python mod_muon_hist.py Data IpIso 2016 postVFP
+python mod_muon_hist.py Data IpIsoSpec 2016 postVFP
+python mod_muon_hist.py MC Id 2016 postVFP
+python mod_muon_hist.py MC IpIso 2016 postVFP
+python mod_muon_hist.py MC IpIsoSpec 2016 postVFP
